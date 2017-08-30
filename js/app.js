2948fe337a0c69e948b33b9a1046467e8014af65
Lev Khruschev <lconnected@techadminpe.alvioneurope.local>
2017-08-30 12:38:52 +0300

    Test: a few angular components refactor

--- js/app.js
+++ js/app.js
@@ -1,13 +1,15 @@
 contextPath = '/';
 appId = 6159447;
 
+/*
 function getQueryStringValue(key) {
     return decodeURI(window.location.search.replace(new RegExp("^(?:.*[&\\?]"
         + encodeURI(key).replace(/[\.\+\*]/g, "\\$&") + "(?:\\=([^&]*))?)?.*$", "i"), "$1"));
-}
+}*/
 
 var availableGroups = [
-    {id: '152271532', name: 'Рукоделие'}
+    {id: '152271532', name: 'Рукоделие'},
+    {id: '152271533', name: 'Рукоделие2'},
 ];
 
 var defaultGroup = availableGroups[0];
@@ -16,18 +18,15 @@ var app = angular.module('app', ['ngRoute']);
 
 app.config(function ($routeProvider) {
     $routeProvider
-
-    // route for the home page
-    //     .when('/', {
-    //         templateUrl: 'templates/message-list.html',
-    //         controller: 'app.messageListController'
-    //     })
-
         .when('/:groupId', {
             templateUrl: 'templates/message-list.html',
             controller: 'app.messageListController'
         })
-
+        .otherwise({
+            redirectTo: '/' + defaultGroup.id,
+            templateUrl: 'templates/message-list.html',
+            controller: 'app.messageListController'
+        });
         // route for the about page
         // .when('/:groupId/one-post', {
         //     templateUrl: 'templates/one-post.html',
@@ -39,10 +38,4 @@ app.config(function ($routeProvider) {
         //     templateUrl: 'templates/search.html',
         //     controller: 'app.searchController'
         // })
-
-        .otherwise({
-            redirectTo: '/' + defaultGroup.id,
-            templateUrl: 'templates/message-list.html',
-            controller: 'app.messageListController'
-        });
 });
--- js/controllers/MessageListController.js
+++ js/controllers/MessageListController.js
@@ -4,13 +4,14 @@
 app.controller('app.messageListController', ['$scope', '$controller', '$routeParams', 'groupService',
     function ($scope, $controller, $routeParams, groupService) {
         // $controller('app.baseListController', {$scope: $scope});
-        $scope.searchApi = function () {
-            return groupService(parseInt($routeParams.groupId))
-                .getMessagesList($scope.messages.length, $scope.pageSize);
-
-        };
-        var groups = groupService(parseInt($routeParams.groupId)).getGroupList();
-        console.log('groups: ' + groups);
+        // $scope.searchApi = function () {
+        //     return groupService(parseInt($routeParams.groupId))
+        //         .getMessagesList($scope.messages.length, $scope.pageSize);
+        //
+        // };
+        var dataSource = groupService($routeParams.groupId);
+        // var groups = groupService(parseInt($routeParams.groupId)).getGroupList();
+        // console.log('groups: ' + groups);
 
         // $scope.getNextPage();
         console.log("app.messageListController controller");
